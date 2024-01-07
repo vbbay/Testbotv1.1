42b3f94004575bede0b104cc5d102006b4574be7
rohan1233424 <155885949+rohan1233424@users.noreply.github.com>
2024-01-07 16:39:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://animedekho123.onrender.com/".format(FQDN)
+    URL = "https://animedekho123-botty.koyeb.app/".format(FQDN)
 else:
-    URL = "https://animedekho123.onrender.com/".format(FQDN)
+    URL = "https://animedekho123-botty.koyeb.app/".format(FQDN)
 
 
 
