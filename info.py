28f5265e38ffdf3740f25817a2a24ad50c25c55c
vbbay <160340655+vbbay@users.noreply.github.com>
2024-02-19 00:33:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/a0aa65161ec712e93
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1315497648').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001824425943').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' -1001824425943').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1315497648').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -66,7 +66,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/OUTFLiX')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/OUTFLiX')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello iam outflix bot')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001971671731'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002106408620'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+kPvF9mqB6GFkOGRl')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
